f9f633dcfb973cd7552c6e3b9f1560a0d952c868
CL3-PC29 <CL3-PC29@DESKTOP-ROLBJ8U>
2025-07-11 14:40:00 +0800

    Sample Program

---
+++ src/main/java/com/mycompany/jirex/Jirex.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jirex;
+
+/**
+ *
+ * @author CL3-PC29
+ */
+public class Jirex {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Fritzgerald Cyrus N Jacinto");
+    }
+}
